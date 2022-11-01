d9edf35861751537beed4a53cd2947f8104c5d90
Arthur Valladares <53414712+vnaj16@users.noreply.github.com>
2022-11-01 16:52:30 -0500

    finish challenge 5

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer')
+const upload = multer({ dest: './uploads/' })
+let bodyParser = require('body-parser')
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post("/api/fileanalyse", function (req, res){
+app.post("/api/fileanalyse", upload.single('upfile'), function (req, res){
   console.log(req)
-  console.log("---------BODY---------")
-  console.log(req.body)
-  res.json({"message": "Building..."})
+  console.log("---------RES---------")
+  console.log(req.file, req.body)
+  let response = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  } 
+
+  console.log("Response", response)
+  res.json(response)
 })
 
 
